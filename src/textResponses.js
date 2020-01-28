41fe224928570315011ebca4327b9e0b5aee0ef5
Daniel Koester <dkoester306@gmail.com>
2020-01-28 01:17:35 -0500

    added all necessary files for completed assignment

--- src/textResponses.js
+++ src/textResponses.js
@@ -1,21 +1,21 @@
-const hello = "Hello World";
+const hello = 'Hello World';
 
-const getTimeString = ()=> {
-    const d = new Date();
-    const dateString = `${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`;
-    return dateString;
+const getTimeString = () => {
+  const d = new Date();
+  const dateString = `${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`;
+  return dateString;
 };
 
-const getTime = (request,response) => {
-    response.writeHead(200,{'Content-type':'text/plain'});
-    response.write(getTimeString());
-    response.end();
+const getTime = (request, response) => {
+  response.writeHead(200, { 'Content-type': 'text/plain' });
+  response.write(getTimeString());
+  response.end();
 };
 
-const getHello = (request,response) => {
-    response.writeHead(200,{'Content-type':'text/plain'});
-    response.write(hello);
-    response.end();
+const getHello = (request, response) => {
+  response.writeHead(200, { 'Content-type': 'text/plain' });
+  response.write(hello);
+  response.end();
 };
 
 module.exports.hello = hello;
